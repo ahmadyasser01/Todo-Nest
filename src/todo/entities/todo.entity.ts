aab1e9c62eff7b8b3d583517c2d6c7cf3738a4f3
ahmadyasser01 <ahmadyasser2814@gmail.com>
2023-04-18 00:15:43 +0200

    fix:add entity decorator to todo

--- src/todo/entities/todo.entity.ts
+++ src/todo/entities/todo.entity.ts
@@ -2,6 +2,7 @@ import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
 import { TODO_STATUS } from '../Enums/todo-status.enum';
 import { User } from 'src/user/entites/user.entity';
 
+@Entity()
 export class Todo {
   @PrimaryGeneratedColumn()
   id: string;
