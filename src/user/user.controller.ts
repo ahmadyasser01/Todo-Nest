a6a63a0172826b614dfc792a3e774b5610fd34c2
ahmadyasser01 <ahmadyasser2814@gmail.com>
2023-04-18 05:39:09 +0200

    feat:config user module to signup

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Post('')
-  createUser(@Body() createUserDto: any) {
+  createUser(@Body() createUserDto: CreateUserDto) {
     return this.userService.createUser(createUserDto);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -10,7 +10,12 @@ export class UserService {
   ) {}
 
   createUser(userData: any) {
+    console.log('test');
     const newUser = this.userRepository.create(userData);
     return this.userRepository.save(newUser);
+  }
+  async findByEmail(email: string): Promise<User | undefined> {
+    console.log('test finding');
+    return this.userRepository.findOne({ where: { email } });
   }
 }
